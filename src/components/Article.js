58abbca054ea799cd97a8a7ad2acbd0a8f4cf0aa
Lucas Henrique <lucasheriques@gmail.com>
2017-03-30 02:56:47 -0300

    Adding Router and Timer

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Timer from './Timer';
+
+class Article extends Component {
+  render() {
+    return (
+      <div>
+        <Timer />
+      </div>
+    );
+  }
+}
+
+export default Article;
---
+++ src/components/Timer.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalSeconds: 59
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      totalSeconds: this.state.totalSeconds + 1
+    })
+  }
+
+  formatSeconds(totalSeconds) {
+    let seconds = totalSeconds % 60;
+    let minutes = Math.floor(totalSeconds / 60);
+
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    return minutes + ':' + seconds;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.formatSeconds(this.state.totalSeconds)}
+      </div>
+    );
+  }
+}
+
+export default Timer;
